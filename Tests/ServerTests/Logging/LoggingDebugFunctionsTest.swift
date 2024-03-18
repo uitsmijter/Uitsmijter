182c7be3fe6b9363533289d2f864a54927c3b66d
Kris Simon <kris@ausdertechnik.de>
2024-03-18 10:57:27 +0100

    #16 | ks | Added tests for the origin of the log message

--- Tests/ServerTests/Logging/LoggingDebugFunctionsTest.swift
+++ Tests/ServerTests/Logging/LoggingDebugFunctionsTest.swift
@@ -8,10 +8,57 @@ final class LoggingDebugFunctionsTest: XCTestCase {
         Log.main = Log(level: Logger.Level.trace)
     }
 
-    func testLogFromThisFunction() {
-        Log.debug("Log from testLogFromThisFunction")
-        XCTAssertEqual(LogWriter.lastLog?.message, "Log from testLogFromThisFunction")
+    func testDebugLogFromThisFunction() {
+        Log.debug("Test log entry")
+        XCTAssertContains(LogWriter.lastLog?.message, "log")
         XCTAssertEqual(LogWriter.lastLog?.level.lowercased(), "debug")
+        XCTAssertEqual("testDebugLogFromThisFunction()", #function)
+
+        XCTAssertContains(LogWriter.lastLog?.function, #function)
+    }
+
+    func testInfoLogFromThisFunction() {
+        Log.info("Test log entry")
+        XCTAssertContains(LogWriter.lastLog?.message, "log")
+        XCTAssertEqual(LogWriter.lastLog?.level.lowercased(), "info")
+        XCTAssertEqual("testInfoLogFromThisFunction()", #function)
+
+        XCTAssertContains(LogWriter.lastLog?.function, #function)
+    }
+
+    func testNoticeLogFromThisFunction() {
+        Log.notice("Test log entry")
+        XCTAssertContains(LogWriter.lastLog?.message, "log")
+        XCTAssertEqual(LogWriter.lastLog?.level.lowercased(), "notice")
+        XCTAssertEqual("testNoticeLogFromThisFunction()", #function)
+
+        XCTAssertContains(LogWriter.lastLog?.function, #function)
+    }
+
+    func testWarningLogFromThisFunction() {
+        Log.warning("Test log entry")
+        XCTAssertContains(LogWriter.lastLog?.message, "log")
+        XCTAssertEqual(LogWriter.lastLog?.level.lowercased(), "warning")
+        XCTAssertEqual("testWarningLogFromThisFunction()", #function)
+
+        XCTAssertContains(LogWriter.lastLog?.function, #function)
+    }
+
+    func testErrorLogFromThisFunction() {
+        Log.error("Test log entry")
+        XCTAssertContains(LogWriter.lastLog?.message, "log")
+        XCTAssertEqual(LogWriter.lastLog?.level.lowercased(), "error")
+        XCTAssertEqual("testErrorLogFromThisFunction()", #function)
+
+        XCTAssertContains(LogWriter.lastLog?.function, #function)
+    }
+
+    func testCriticalLogFromThisFunction() {
+        Log.critical("Test log entry")
+        XCTAssertContains(LogWriter.lastLog?.message, "log")
+        XCTAssertEqual(LogWriter.lastLog?.level.lowercased(), "critical")
+        XCTAssertEqual("testCriticalLogFromThisFunction()", #function)
+
         XCTAssertContains(LogWriter.lastLog?.function, #function)
     }
 }
