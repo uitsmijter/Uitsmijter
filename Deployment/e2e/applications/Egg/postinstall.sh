cb9abf3f936360a615fa0e2bb451708ee8aa01ea
Kris Simon <kris@ausdertechnik.de>
2025-03-08 17:55:38 +0100

    UIT-578 | ks | wait for e2e application to be online

--- Deployment/e2e/applications/Egg/postinstall.sh
+++ Deployment/e2e/applications/Egg/postinstall.sh
@@ -3,4 +3,4 @@
 TIMEOUT=${K8S_TIMEOUT:-"5m"}
 
 # Wait until the cookbooks web server pod is ready to use
-kubectl wait --for=condition=ready pod --timeout=${TIMEOUT} --selector=app=yolk-webserver -n egg
+kubectl wait --for=condition=ready pod --timeout=${TIMEOUT} --selector=app=yolk-webserver --selector=app=glair-webserver -n egg
