e4f40e0dcddbe7318655ac0f9dd1878a9e8f322e
Kris Simon <kris@ausdertechnik.de>
2025-11-04 16:47:04 +0100

    fix e2e tests on bnbc clients

--- Tests/e2e/playwright/tests/OAuth/SilentLogin.spec.ts
+++ Tests/e2e/playwright/tests/OAuth/SilentLogin.spec.ts
@@ -43,7 +43,7 @@ test.describe('OAuth flow - Silent login enabled', () => {
         // General Auth 2 Flow - Step 1 - 2
         test('should respond with a login page to the request for an authorization code', async () => {
             const response = await authorizeApiRequest(
-                'https://id.example.com',
+                'https://login.bnbc.example',
                 {
                     response_type: 'code',
                     client_id: 'e942df47-4810-4a9a-8f25-47e8cf03325d',
@@ -55,10 +55,11 @@ test.describe('OAuth flow - Silent login enabled', () => {
                     code_challenge_method: 'S256'
                 }
             )
-
             await expect(response.headers()['server']).toContain('Uitsmijter')
             await expect(response.status()).toBe(401)
-
+            await expect(response.url()).toContain('/authorize')
+            await expect(response.url()).toContain('response_type=code')
+            
             const content = await response.text()
             await expect(content).toContain('form action="/login"')
         })
@@ -67,14 +68,16 @@ test.describe('OAuth flow - Silent login enabled', () => {
         test('should respond after login with a code for authorization', async ({page}) => {
             const response = await loginAuthorizeFormRequest(
                 page,
-                'https://id.example.com',
+                'https://login.bnbc.example',
                 {
+                    response_type: "code",
                     client_id: "e942df47-4810-4a9a-8f25-47e8cf03325d",
                     redirect_uri: "https://api1.bnbc.example/",
-                    response_type: "code",
                     scope: "access",
                     state: "" + myState,
-                    username: "cee8Esh5@example.com"
+                    username: "cee8Esh5@example.com",
+                    code_challenge: codeChallenge,
+                    code_challenge_method: 'S256'
                 }
             )
 
@@ -96,7 +99,7 @@ test.describe('OAuth flow - Silent login enabled', () => {
         test('should respond with a redirect back to the requesting page with an authorization code', async ({page}) => {
             const response = await authorizeApiRequestOnPage(
                 page,
-                'https://id.example.com',
+                'https://login.bnbc.example',
                 {
                     response_type: 'code',
                     client_id: 'e942df47-4810-4a9a-8f25-47e8cf03325d',
@@ -120,14 +123,16 @@ test.describe('OAuth flow - Silent login enabled', () => {
         test('different client should respond with a redirect back to the requesting page with an authorization code', async ({page}) => {
             const response = await authorizeApiRequestOnPage(
                 page,
-                'https://id.example.com',
+                'https://login.bnbc.example',
                 {
                     response_type: 'code',
                     client_id: '4ad5d978-98b1-436b-8df6-316d700cf8f2',
                     client_secret: null,
                     redirect_uri: 'https://api2.bnbc.example/',
                     scope: '',
-                    state: '' + myState
+                    state: '' + myState,
+                    code_challenge: codeChallenge,
+                    code_challenge_method: 'S256'
                 }
             )
 
