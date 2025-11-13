8c09c2e455061168c6eb994cec59dadf01041fe5
Kris Simon <kris@ausdertechnik.de>
2025-11-13 18:58:09 +0100

    Should pass tests

--- Tests/Uitsmijter-AuthServerTests/AppTests.swift
+++ Tests/Uitsmijter-AuthServerTests/AppTests.swift
@@ -10,7 +10,6 @@ struct AppTests {
         try await withApp(configure: configure) { app in
             try await app.testing().test(.GET, "", afterResponse: { @Sendable res async throws in
                 #expect(res.status == .ok)
-                #expect("should fail" == "for testing")
             })
         }
     }
